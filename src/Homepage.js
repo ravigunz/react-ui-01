6f9ee70f3157272b54c639ddd1f04ffe36bdd565
Ravi Gunasena <ravigunz@gmail.com>
2023-03-16 14:20:26 +0530

    project initiate

---
+++ src/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div style={{display: 'flex', padding: '10px', margin: '10px'}}>
+        <h1 style={{color: 'green'}}>Homepage</h1>
+    </div>
+  )
+}
